c983b5f57bb34105bf35bf0bbb4b2c64ed6d59b9
Ruslan <ruslan.postoyuk@gmail.com>
2022-01-20 20:35:21 +0200

    7. Работа с формой-калькулятором, часть 2

--- src/js/main.js
+++ src/js/main.js
@@ -16,5 +16,23 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    forms(modalState);
+    forms(modalState); /* Что бы отправляемая форма знала о тех данных которые пользователь ввел на странице
+    нам необходимо передать modalState
+    И так же само заходим в forms и туда тоже передаем какой-то states с которым сможем работать внутри
+    Наш state который мы передали в формы работает только в самом конце при отправка формы 
+    array(7) {
+  ["user_name"]=>
+  string(15) "Ruslan Postoiuk"
+  ["user_phone"]=>
+  string(11) "48576381569"
+  ["type"]=>
+  string(6) "french" тут или пластиковое, древесиное , т.е люьбой селект
+  ["form"]=>
+  string(1) "2"
+  ["width"]=>
+  string(3) "200"
+  ["height"]=>
+  string(3) "600"
+  ["profile"]=>
+  string(16) "Холодное"*/
 });
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,6 +1,6 @@
 import checkNumInputs from "./checkNumInputs";
 
-const forms = () => {
+const forms = (state) => { //сюда
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input');
     
@@ -36,7 +36,22 @@ const forms = () => {
             statusMessage.classList.add('status');
             item.appendChild(statusMessage);
 
-            const formData = new FormData(item);
+            const formData = new FormData(item); // все данные 
+            if(item.getAttribute("data-calc") === "end" ){/* если у формы которую ты пытаешься сейчас 
+отправить есть определенный атрибут, то мы еще добавим определенных данных. Мы проверяем а точно ли это то модельное
+окно которое нас интересует(помни что мы атрибут end добавили в индекс html, именна та форма которая находится
+в конце)*/
+
+                /*Если это действительно так то мы берем наш объект state, который есть внутри этой функции и мы
+его разбираем на отдельные ключи и значения (как это было в консоли, т.е form и 1 к примеру, width и 500,
+высота такая и какое-то значение и так далее) и каждые эти пары мы будем аппендить(добавлять) во внутрь формдейты.
+ПОТОМУ ЧТО ПРЯМО СЕЙЧАС ФОРМ ДАТА СОДЕРЖИТ ТОЛЬКО ИМЯ ПОЛЬЗОВАТЕЛЯ И НОМЕР ТЕЛЕФОНА И НЕ ЗНАЕТ ЧТО ПОЛЬЗОВАТЕЛЬ ТАМ
+НАТЫКАЛ*/
+                for(let key in state){ /* так мы берем те данные из state что уже сформировались, мы их перебираем */
+                    formData.append(key, state[key]);/* Так мы их отправляем. Во время добавляение данных через 
+аппенд мы задаем методу два аргумента это сначало значение (key) и ключ(state[key]) */
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
